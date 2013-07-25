581b195f25a740564afcaed64fc50748094c11ba
Chris Dickinson <christopher.s.dickinson@gmail.com>
2013-07-25 16:13:39 -0700

    Rework to be a library.

--- index.js
+++ index.js
@@ -1,342 +1,237 @@
-var process = require('process')
-  , path = require('path')
-  , fs = require('fs')
+var lang = require('cssauron-falafel')
 
-var falafel = require('falafel')
-  , language = require('cssauron-falafel')
-  , colors = require('ansicolors')
-  , concat = require('concat-stream')
-  , optimist = require('optimist')
+var is_scope_creation = lang('function, catch, [has_let_statements], :root')
+  , is_let_scope = lang(
+      'function, block, for, for-in, [type=ForOfStatement], :root'
+    )
+  , is_neutral_id = lang(
+      'object > * > id:first-child, ' +
+      'lookup > * + id, function > id, catch > id'
+    )
+  , is_declaration = lang('variable > id:first-child')
+  , is_assign = lang('assign > id:first-child')
+  , is_function = lang('function, :root')
+  , is_root = lang(':root')
+  , is_ident = lang('id')
 
-var target = (optimist.argv._ || [])[0]
-  , cwd = process.cwd()
-  , output
-  , input
+module.exports = function(ignore, ready) {
+  var scopes = []
 
-var GLOBALS = [
-   'module'
- , 'arguments'
- , 'this'
- , 'process'
- , 'console'
- , 'setInterval'
- , 'clearInterval'
- , 'setTimeout'
- , 'clearTimeout'
- , 'JSON'
- , 'require'
- , 'window'
- , 'Math'
- , 'Object'
- , 'Function'
- , 'Number'
- , 'Array'
- , 'RegExp'
- , 'Date'
- , 'document'
- , 'define'
- , 'undefined'
- , 'String'
- , 'Boolean'
- , 'eval'
- , 'encodeURIComponent'
- , 'decodeURIComponent'
- , 'unescape'
- , 'parseInt'
- , 'parseFloat'
- , 'TypeError'
- , 'SyntaxError'
- , 'Error'
- , 'Image'
- , 'isNaN'
- , 'Infinity'
- , 'NaN'
- , 'isFinite'
- , 'navigator'
- , 'Uint8Array'
- , 'Uint16Array'
- , 'Uint32Array'
- , 'Int8Array'
- , 'Int16Array'
- , 'Int32Array'
- , 'Float32Array'
- , 'Float64Array'
- , 'XMLHttpRequest'
-]
-
-if(!target) {
-  return help()
-}
-
-if(target === '-') {
-  process.stdin.pipe(concat(process_file))
-  try { process.stdin.resume() } catch(err) { }
-  return
-} else {
-  target = target[0] === '/' ? target : path.join(cwd, target || '')
-  try {
-    input = fs.createReadStream(target).pipe(concat(process_file))
-    return
-  } catch(err) {
-    console.error('not a file')
+  if(arguments.length === 1) {
+    ready = ignore
+    ignore = []
   }
-}
-process.exit(1)
 
-function process_file(input) {
-  var is_assign = language('assign > id:first-child')
-    , is_var = language('variable > id:first-child')
-    , is_func = language('function > id:first-child')
-    , func = language('function')
-    , id = language('id')
-    , rhs_lookup = language('lookup > * + id')
-    , obj_expr = language('object > * > id:first-child')
-    , catch_clause = language('catch')
-    , try_clause = language('try')
+  return write_node
 
-  try {
-    input = '(function(){'+input+'})'
-
-    var innermost_node
-      , pos = null
-
-    if(optimist.argv.position) {
-      pos = line_col_to_idx.apply(null, (optimist.argv.position+'').split(',').map(Number))
-      if(pos.length < 1) {
-        pos.push(1)
-      }
+  function write_node(node) {
+    switch(true) {
+      case is_root(node): finish(node); break
+      case is_declaration(node): decl(node); break
+      case is_scope_creation(node): exit(node); break
+      case is_ident(node) && !is_neutral_id(node): use(node); break
     }
-    output = falafel(input, function(node) {
-      var scope = node
-        , idx
-
-      if(node.range[0] <= pos && node.range[1] >= pos) {
-        if(!innermost_node || (node.range[1] - node.range[0] < innermost_node.range[1] - innermost_node.range[0])) {
-          innermost_node = node
-        }
-      }
-
-      if(is_var(node) || is_func(node)) {
-        while(scope && !func(scope)) {
-          scope = scope.parent
-        }
-
-        init(scope)
-        scope.declare(node)
-      } else if(func(node) || catch_clause(node)) {
-        scope = node.parent
-        while(scope && !(func(scope) || catch_clause(node))) {
-          scope = scope.parent
-        }
-        
-        if(!scope) {
-          output(node.unresolved)
-          return
-        }
-
-        init(scope)
-        if(node.id && /Declaration/.test(node.type)) {
-          scope.declare(node.id)
-        }
-
-        init(node)
-        node.unresolved.forEach(function(name) {
-          scope.use(name, name._kind)
-        })
-      } else if(id(node) && !rhs_lookup(node) && !obj_expr(node)) {
-        while(scope && !(func(scope) || catch_clause(scope))) {
-          scope = scope.parent
-        }
-
-        init(scope)
-        scope.use(node, is_assign(node) ? 'explicit' : 'implicit')
-      }
-    })
-  } catch(err) {
-    console.error('invalid javascript:', err.stack)
-    return
   }
 
-  function init(scope) {
-    if(scope.use) {
+  function decl(node) {
+    var is_let = node.parent.type === 'VariableDeclarator'
+      , parent
+      
+    is_let = node.parent.parent.kind === 'let'
+
+    parent = get_parent(node, is_let ? is_let_scope : is_function)
+  
+    if(is_let) {
+      parent.has_let_statements = true
+    }
+
+    get_scope(parent).declare(node)
+  }
+
+  function exit(node) {
+    var parent = get_parent(node.parent, is_scope_creation)
+      , this_scope
+      , scope
+
+    this_scope = get_scope(node)
+
+    if(!parent) {
+      return finish(node)
+    }
+
+    scope = get_scope(parent)
+
+    // pull function name out into containing
+    // scope iff this node is a declaration to
+    // handle named function statement hoisting
+
+    if(node.id && node.type === 'FunctionDeclaration') {
+      scope.declare(node.id)
+    }
+
+    scope.consume(this_scope)
+  }
+
+  function use(node) {
+    if(ignore.indexOf(node.name) !== -1) {
       return
     }
 
-    scope.declared = scope.declared || make_decl(scope)
-    scope.unresolved = scope.unresolved || []
-    scope.declare = function(node) {
-      node._count = 1 
-      scope.unresolved = scope.unresolved.filter(not_matches(node, 1))
-      if(scope.declared.some(matches(node))) {
-        return
-      }
-      scope.declared.push(node)
+    var parent = get_parent(node.parent, is_scope_creation)
+      , scope
+
+    if(!parent) {
+      return finish(node)
     }
 
-    scope.use = function(node, kind) {
-      node._count = 1 
-      if(scope.declared.some(matches(node, 1)) || scope.unresolved.some(matches(node, 1))) {
-        for(var i = 0, len = scope.declared.length; i < len; ++i) {
-          if(node.name === scope.declared[i].name) {
-            ++scope.declared[i]._count
-            break
-          }
-        }
-        return
+    scope = get_scope(parent)
+    scope.use(node)
+  }
+
+  function get_scope(node) {
+    for(var i = 0, len = scopes.length; i < len; ++i) {
+      if(scopes[i].defined_by === node) {
+        return scopes[i]
       }
-      if(GLOBALS.indexOf(node.name) !== -1) {
-        return
+    }
+
+    return scopes.unshift(make_scope(node)), scopes[0]
+  }
+
+  function make_scope(node) {
+    var scope = {
+        consume: scope_consume
+      , defined_by: node
+      , use: scope_use
+      , declared: scope_make_decl(node)
+      , declare: scope_declare
+      , unresolved: []
+      , children: []
+    }
+
+    return scope
+  }
+
+  function scope_declare(node) {
+    var scope = this
+      , promoted
+
+    if(scope.declared.some(matches(node))) {
+      return
+    }
+
+    scope.unresolved = scope.unresolved.filter(function(item) {
+      if(item.name === node.name) {
+        promoted = item
       }
-      node._kind = kind
-      node._count = node._count || 0
-      scope.unresolved.push(node)
+
+      return item.name !== node.name
+    })
+
+    if(promoted) {
+      promoted.nodes.push({
+          node: node
+        , kind: 'declare'
+      })
+    }
+
+    scope.declared.push(
+        promoted || {name: node.name, nodes: [{node: node, kind: 'declare'}]}
+    )
+
+    function not_match(item) {
+      return item.name !== node.name
     }
   }
 
-  function matches(node, incr) {
-    return function(inner) {
-      return inner.name === node.name
-    }
-  }
+  function scope_make_decl(node) {
+    var out = node.params ? node.params :
+              node.param ? [node.param] :
+              []
+  
+    out = out.slice()
 
-  function not_matches(node, apply) {
-    return function(inner) {
-      return inner.name !== node.name
+    if(node.id && node.type === 'FunctionExpression') {
+      out.push(node.id)
     }
-  }
 
-  function make_decl(fn) {
-    var out = (fn.params || (fn.param ? [fn.param] : [])).slice()
-
-    if(fn.id) {
-      out.push(fn.id)
-    }
+    out = out.map(function(node) {
+      return {name: node.name, nodes: [{node: node, kind: 'declare'}]}
+    })
 
     return out
   }
 
-  function output(unresolved) {
-    var explicit = 0
-      , ident
-      , range
+  function scope_use(node) {
+    var scope = this
+      , kind
 
-    for(var i = 0, len = unresolved.length; i < len; ++i) {
-      ident = unresolved[i]
-
-      range = ident.range.map(idx_to_line_col)
-      if(ident._kind === 'explicit') {
-        ++explicit
+    kind = is_assign(node) ? 'explicit' : 'implicit'
+   
+    for(var i = 0, len = scope.declared.length; i < len; ++i) {
+      if(node.name !== scope.declared[i].name) {
+        continue
       }
-      console.log(
-        range[0]
-      + ', '+ident._kind+': '+colors.red(ident.name)
-      )
+
+      scope.declared[i].nodes.push({node: node, kind: kind})
+
+      return
     }
 
-    if(pos === null) {
-      return process.exit(explicit ? 1 : 0)
+    for(var i = 0, len = scope.unresolved.length; i < len; ++i) {
+      if(node.name !== scope.unresolved[i].name) {
+        continue
+      }
+
+      scope.unresolved[i].nodes.push({node: node, kind: kind})
+
+      return
     }
 
-    var scope = innermost_node
-    while(scope) {
-      while(scope && !scope.declared) scope = scope.parent
-      if(!scope) return
-
-      var from = idx_to_line_col(scope.range[0])
-        , to = idx_to_line_col(scope.range[1])
-
-      if(catch_clause(scope)) {
-        console.log(colors.yellow('<catch clause>')+' from '+from+' to '+to)
-      } else if(try_clause(scope)) {
-        console.log(colors.yellow('<try clause>')+' from '+from+' to '+to)
-      } else {
-        console.log(colors.yellow(scope.id ? '<function '+scope.id.name+'>' : '<anonymous>') +' from '+from+' to '+to)
-      }
-
-      for(var i = 0, len = scope.declared.length; i < len; ++i) {
-        console.log(
-          colors.green('+ ')+pad(scope.declared[i].name, 24)+' used '+
-          colors.magenta(scope.declared[i]._count)+' times (from '+idx_to_line_col(scope.declared[i].range[0])+')'
-        )
-      }
-
-      for(var i = 0, len = scope.unresolved.length; i < len; ++i) {
-        console.log(
-          colors.magenta('* ')+pad(scope.unresolved[i].name, 24)+' used '+
-          colors.magenta(scope.unresolved[i]._count)+' times (from '+idx_to_line_col(scope.unresolved[i].range[0])+')'
-        )
-
-      }
-
-      scope = scope.parent
-    }
-
-    return process.exit(explicit ? 1 : 0)
+    scope.unresolved.push({name: node.name, nodes: [{node: node, kind: kind}]})
   }
 
-  function pad(s, n) {
-    while(s.length < n) s = s+' '
-    return s.slice(0, n)
+  function scope_consume(other_scope) {
+    var scope = this
+      , nodes
+
+    scopes.splice(scopes.indexOf(other_scope), 1)
+    scope.children.push({
+        scope: other_scope.defined_by
+      , vars: other_scope.declared
+      , uses: other_scope.unresolved
+      , children: other_scope.children  
+    })
+
+    for(var i = 0, len = other_scope.unresolved.length; i < len; ++i) {
+      nodes = other_scope.unresolved[i].nodes
+
+      for(var j = 0, jlen = nodes.length; j < jlen; ++j) {
+        scope.use(nodes[j].node)
+      }
+    } 
   }
 
-  function line_col_to_idx(line, col) {
-    line -= 1
-    col -= 1
-
-    for(var i = 0, len = input.length; i < len; ++i) {
-      if(line && input[i] === '\n') {
-        --line
-      }
-      if(!line) {
-        if(!col) {
-          return i
-        }
-        --col
-      }
-    }
-    return null
+  function finish(node) {
+    ready({
+        scope: scopes[0].defined_by
+      , vars: scopes[0].declared
+      , uses: scopes[0].unresolved
+      , children: scopes[0].children  
+    })
   }
 
-  function idx_to_line_col(idx) {
-    var lines = 0
-      , columns = 0
-
-    for(var i = 0, len = idx; i < len; ++i) {
-      ++columns
-      if(input[i] === '\n') {
-        ++lines
-        columns = 0
-      }
-    }
-
-    return {line: 1 + lines, col: columns + 1, toString: str}
-
-    function str() {
-      return 'line '+colors.cyan(this.line)+', col '+colors.cyan(this.col)
+  function matches(node) {
+    return function(inner) {
+      return inner.name === node.name
     }
   }
 }
 
-function help() {
-/*
-scoped [path/to/file.js|-] [--position=LINE[,COLUMN]]
+function get_parent(node, sel) {
+  while(node && !sel(node)) {
+    node = node.parent
+  } 
 
-  output global variable usage / leakage information.
-
-  if `position` is provided, additionally output a scope usage chain.
-
-  if there are any explicit globals, the command will exit with a return code of `1`.
-
-  arguments:
-
-    --position LINE[,COLUMN]    create a usage chain for the function containing `LINE` and 
-                                `COLUMN`. If `COLUMN` is not given, it is assumed to be `1`.
-
-                                Both `LINE` and `COLUMN` are 1-indexed.
-
-*/
-
-  var str = help+''
-
-  process.stdout.write(str.slice(str.indexOf('/*')+3, str.indexOf('*/')))
+  return node
 }
